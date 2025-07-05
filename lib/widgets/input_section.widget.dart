9d1e8cd6d87c0bc0265fecc6751103a0c1c11765
Khai Dam <quangkhaidam93@gmail.com>
2025-07-05 10:47:16 +0700

    fix: remove unused import

--- lib/widgets/input_section.widget.dart
+++ lib/widgets/input_section.widget.dart
@@ -1,5 +1,3 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-
 import 'package:flutter/material.dart';
 
 import '../controllers/mention_input_text_editing.controller.dart';
